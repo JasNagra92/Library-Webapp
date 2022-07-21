ae0955eea7be8789684135ce5f60c32c48b24934
jasmac <jaskarannagra@hotmail.com>
2022-07-21 12:56:52 -0700

    change for loop to for Each loop when filtered over Library to not add dupes to table

--- js.js
+++ js.js
@@ -1,30 +1,34 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages + " pages"
-    this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages + " pages";
+  this.read = read;
+  this.inTable = "false";
 }
 
-function addBookToLibrary(book){ 
-    myLibrary.push(book)
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
-let hobbit = new Book('The Hobbit', "J.R. Tolkien", 300, 'read');
+let hobbit = new Book("The Hobbit", "J.R. Tolkien", 300, "read");
 addBookToLibrary(hobbit);
 
-function displayBooks(array){
-    
-    for (let i = 0; i < array.length;i++){
-        let table = document.getElementById("booktable");
-        let row = table.insertRow(1);
-        let title = row.insertCell(0);
-        let author = row.insertCell(1);
-        let pages = row.insertCell(2);
-        let read = row.insertCell(3);
-        title.innerHTML = array[i].title;
-        author.innerHTML = array[i].author;
-        pages.innerHTML = array[i].pages;
-        read.innerHTML = array[i].read
-     }
+function displayBooks(array) {
+  array.forEach((book) => {
+    let table = document.getElementById("booktable");
+
+    if (book.inTable == "false") {
+      let row = table.insertRow(1);
+      let title = row.insertCell(0);
+      let author = row.insertCell(1);
+      let pages = row.insertCell(2);
+      let read = row.insertCell(3);
+      title.innerHTML = book.title;
+      author.innerHTML = book.author;
+      pages.innerHTML = book.pages;
+      read.innerHTML = book.read;
+      book.inTable = "true";
+    }
+  });
 }
